a3eaa64c3f0759dc0db1da0ace836cfaf8b6c245
souhail.hajji <souhail.hajji@sesame.com.tn>
2024-12-24 22:07:41 +0100

    - Add enum Mois - Validator for annee - Follow best practice for endpoint

--- src/main/java/com/example/demo/controllers/SalaireController.java
+++ src/main/java/com/example/demo/controllers/SalaireController.java
@@ -2,20 +2,47 @@ package com.example.demo.controllers;
 
 import com.example.demo.entities.Salaire;
 import com.example.demo.services.ISalaireService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("salaire")
 public class SalaireController {
-    @Autowired
-    ISalaireService salaireService;
-    @PostMapping("saveSalaire")
-    public Salaire saveOrUpdateSalaire(@RequestBody Salaire salaire) {
 
+    private final ISalaireService salaireService;
+
+    private SalaireController(ISalaireService salaireService) {
+        this.salaireService = salaireService;
+    }
+
+    @PostMapping
+    public Salaire saveSalaire(@RequestBody Salaire salaire) {
         return salaireService.saveOrUpdateSalaire(salaire);
+    }
+
+    @PutMapping
+    public Salaire updateSalaire(@RequestBody Salaire salaire) throws Exception {
+        return salaireService.saveOrUpdateSalaire(salaire);
+    }
+
+    @GetMapping("/id/{id}")
+    public Salaire getSalaireById(@PathVariable int id) {
+        return salaireService.getSalaireById(id);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Salaire>> getAllSalaires() {
+        List<Salaire> salaires = salaireService.getAllSalaires();
+        if (salaires.isEmpty())
+            return ResponseEntity.noContent().build();
+        return ResponseEntity.ok(salaires);
+    }
+
+    @DeleteMapping("/id/{id}")
+    public ResponseEntity<String> deleteById(@PathVariable int id) {
+        salaireService.deleteSalaire(id);
+        return ResponseEntity.ok("Supprimé");
     }
 }
--- src/main/java/com/example/demo/entities/Salaire.java
+++ src/main/java/com/example/demo/entities/Salaire.java
@@ -1,10 +1,14 @@
 package com.example.demo.entities;
 
+import com.example.demo.Enum.Mois;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.antlr.v4.runtime.misc.NotNull;
 
 @Entity
 @Getter
@@ -17,7 +21,9 @@ public class Salaire {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private int mois;
+    private Mois mois;
+    @Min(value = 1900, message = "Year must be at least 1900")
+    @Max(value = 2100, message = "Year must not exceed 2100")
     private int annee;
     private float montantBrut;
     private float montantNet;
