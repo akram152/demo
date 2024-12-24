a86e6de81b3af03116f061a9ec3a4a3c81902f63
Jaoua Akram <akram.jaoua02@gmail.com>
2024-12-24 23:22:06 +0100

    initial commit

--- src/main/java/com/example/demo/controllers/CollaborateurController.java
+++ src/main/java/com/example/demo/controllers/CollaborateurController.java
@@ -3,20 +3,47 @@ package com.example.demo.controllers;
 import com.example.demo.entities.Collaborateur;
 import com.example.demo.services.ICollaborateurService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("collaborateur")
 public class CollaborateurController {
     @Autowired
     ICollaborateurService collaborateurService;
+
     @PostMapping("addCollaborateur")
     public Collaborateur addCollaborateur(@RequestBody Collaborateur collaborateur) {
         return collaborateurService.addCollaborateur(collaborateur);
     }
+
+    @GetMapping("getById/{id}")
+    public Collaborateur getCollaborateurById(@PathVariable int id) {
+        return collaborateurService.getCollaborateurById(id);
+    }
+
+    @GetMapping("getAllCollab")
+    public List<Collaborateur> getAllCollab() {
+        return  collaborateurService.getAllCollaborateurs();
+    }
+
+    @PutMapping("PutCollab")
+    public Collaborateur putCollaborateur(@RequestBody Collaborateur collaborateur) {
+        return collaborateurService.updateCollaborateur(collaborateur);
+    }
+
+    @DeleteMapping("deleteById/{id}")
+    public void deleteCollaborateur(@PathVariable("id") int id) {
+        collaborateurService.deleteCollaborateur(id);
+    }
+
+
+
+
+
+
+
 
 
 }
--- src/main/java/com/example/demo/services/ICollaborateurService.java
+++ src/main/java/com/example/demo/services/ICollaborateurService.java
@@ -2,6 +2,12 @@ package com.example.demo.services;
 
 import com.example.demo.entities.Collaborateur;
 
+import java.util.List;
+
 public interface ICollaborateurService {
     public Collaborateur addCollaborateur(Collaborateur collaborateur);
+    public Collaborateur getCollaborateurById(int id);
+    public List<Collaborateur> getAllCollaborateurs();
+    public Collaborateur updateCollaborateur(Collaborateur collaborateur);
+    public void deleteCollaborateur(int id);
 }
