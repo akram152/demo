5c69651327eff6f08de753ec0ab7bdaacf5afab9
souhail.hajji <souhail.hajji@sesame.com.tn>
2024-12-26 16:24:07 +0100

    - Add enum Mois - Validator for annee - Follow best practice for endpoint -Add DepartmentController -Add DepartmentServiceImpl -Add iDepartmentRepository -Add IDepartmentService

--- src/main/java/com/example/demo/controllers/SalaireController.java
+++ src/main/java/com/example/demo/controllers/SalaireController.java
@@ -1,55 +1,9 @@
 package com.example.demo.controllers;
 
-import com.example.demo.Enum.Mois;
-import com.example.demo.entities.Departement;
-import com.example.demo.entities.Salaire;
-import com.example.demo.services.Interf.ISalaireService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("salaire")
 public class SalaireController {
-
-    private final ISalaireService salaireService;
-
-    private SalaireController(ISalaireService salaireService) {
-        this.salaireService = salaireService;
-    }
-
-    @PostMapping
-    public Salaire saveSalaire(@RequestBody Salaire salaire) {
-        return salaireService.saveOrUpdateSalaire(salaire);
-    }
-
-    @PutMapping
-    public Salaire updateSalaire(@RequestBody Salaire salaire) throws Exception {
-        return salaireService.saveOrUpdateSalaire(salaire);
-    }
-
-    @GetMapping("/id/{id}")
-    public Salaire getSalaireById(@PathVariable int id) {
-        return salaireService.getSalaireById(id);
-    }
-
-    @GetMapping
-    public ResponseEntity<List<Salaire>> getAllSalaires() {
-        List<Salaire> salaires = salaireService.getAllSalaires();
-        if (salaires.isEmpty())
-            return ResponseEntity.noContent().build();
-        return ResponseEntity.ok(salaires);
-    }
-
-    @GetMapping("getbymoisandannee/{mois}/{annee}")
-    public List<Salaire> getByMoisannee(@PathVariable Mois mois, @PathVariable int annee) {
-        return salaireService.findSalaireByMoisAndAnnee(mois, annee);
-    }
-
-    @DeleteMapping("/id/{id}")
-    public ResponseEntity<String> deleteById(@PathVariable("id") int id) {
-        salaireService.deleteSalaire(id);
-        return ResponseEntity.ok("Supprimé");
-    }
 }
